dae6b2ea8b5a37ede7808d424971322f33c04ba0
AsemAlhaidary <asemalhaidary.2001@gmail.com>
2022-05-09 00:44:55 +0300

    database

--- db/buildingDB.sql
+++ db/buildingDB.sql
@@ -16,27 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `contractors`
---
-
-DROP TABLE IF EXISTS `contractors`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `contractors` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `contractor_name` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
-  `contractor_work_details` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
-  `contractor_work_unit` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
-  `contractor_work_quantity` int(50) DEFAULT NULL,
-  `contractor_unit_price` decimal(40,2) DEFAULT NULL,
-  `contractor_work_total` decimal(60,2) DEFAULT NULL,
-  `project_id` int(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_contractors_projects1_idx` (`project_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `contractors`
 --
 
@@ -47,54 +26,14 @@ INSERT INTO `contractors` VALUES (1,'Ehab','صبة 50 متر','متر مربع',
 UNLOCK TABLES;
 
 --
--- Table structure for table `employees`
---
-
-DROP TABLE IF EXISTS `employees`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `employees` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `employee_name` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `employee_job` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
-  `employee_phone_num` varchar(300) CHARACTER SET utf8 DEFAULT NULL,
-  `employee_day_price` decimal(60,2) DEFAULT NULL,
-  `employee_start_date` date DEFAULT NULL,
-  `employee_end_date` date DEFAULT NULL,
-  `project_id` int(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_employees_projects1_idx` (`project_id`),
-  CONSTRAINT `fk_employees_projects1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `employees`
 --
 
 LOCK TABLES `employees` WRITE;
 /*!40000 ALTER TABLE `employees` DISABLE KEYS */;
-INSERT INTO `employees` VALUES (6,'Asem','Programmer','+967775690990',NULL,NULL,NULL,10),(7,'Ehab','Man','+966564080144',NULL,NULL,NULL,10),(8,'Asem','Programmer','+967775690990',3000.00,'2022-05-09','2022-05-17',10);
+INSERT INTO `employees` VALUES (9,'Asem','Programmer','+967775690990',5000.00,'2022-05-04','2022-05-19',15.00,10),(10,'Ehab','Man','+966564080144',3000.00,'2022-05-01','2022-07-06',198000.00,10),(11,'Ehab','Man','+966564080144',2000.00,'2022-05-18','2022-05-18',0.00,11),(12,'ASEM F. ALHAIDARI','البطل','+967775690990',3000.00,'2022-05-10','2022-05-08',-6000.00,11),(13,'Father','Man','+966564080144',3000.00,'2022-05-23','2022-06-08',48000.00,11);
 /*!40000 ALTER TABLE `employees` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `extras`
---
-
-DROP TABLE IF EXISTS `extras`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `extras` (
-  `id` int(11) NOT NULL,
-  `extra_time` decimal(60,2) DEFAULT NULL,
-  `extra_time_price` decimal(60,2) DEFAULT NULL,
-  `extra_total_price` decimal(60,2) DEFAULT NULL,
-  `employee_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_extras_employees1_idx` (`employee_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `extras`
@@ -102,32 +41,9 @@ CREATE TABLE `extras` (
 
 LOCK TABLES `extras` WRITE;
 /*!40000 ALTER TABLE `extras` DISABLE KEYS */;
-INSERT INTO `extras` VALUES (2,5.00,3000.00,15000.00,6),(4,43.00,433.00,18619.00,6),(5,6.00,5000.00,30000.00,7);
+INSERT INTO `extras` VALUES (2,5.00,3000.00,15000.00,NULL),(4,43.00,433.00,18619.00,NULL),(5,6.00,5000.00,30000.00,NULL);
 /*!40000 ALTER TABLE `extras` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `invoices`
---
-
-DROP TABLE IF EXISTS `invoices`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `invoices` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `invoice_name` varchar(200) DEFAULT NULL,
-  `invoice_details` varchar(200) DEFAULT NULL,
-  `invoice_number` int(11) DEFAULT NULL,
-  `invoice_unit` varchar(200) DEFAULT NULL,
-  `invoice_unit_price` decimal(20,2) DEFAULT NULL,
-  `invoice_unit_qantity` int(11) DEFAULT NULL,
-  `invoice_total` decimal(20,2) DEFAULT NULL,
-  `projects_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_invoices_projects1_idx` (`projects_id`),
-  CONSTRAINT `fk_invoices_projects1` FOREIGN KEY (`projects_id`) REFERENCES `projects` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `invoices`
@@ -137,26 +53,6 @@ LOCK TABLES `invoices` WRITE;
 /*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
 /*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `managers`
---
-
-DROP TABLE IF EXISTS `managers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `managers` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `manager_name` varchar(200) DEFAULT NULL,
-  `manager_outlay_details` varchar(200) DEFAULT NULL,
-  `manager_outlay_amount` decimal(60,2) DEFAULT NULL,
-  `manager_outlay_date` date DEFAULT NULL,
-  `project_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_managers_outlays_projects1_idx` (`project_id`),
-  CONSTRAINT `fk_managers_outlays_projects1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `managers`
@@ -169,56 +65,14 @@ INSERT INTO `managers` VALUES (1,'Asemhrth','hdfhfshfd',666666.00,'2022-05-09',1
 UNLOCK TABLES;
 
 --
--- Table structure for table `projects`
---
-
-DROP TABLE IF EXISTS `projects`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `projects` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_name` varchar(100) DEFAULT NULL,
-  `project_address` varchar(2000) DEFAULT NULL,
-  `project_start_date` varchar(40) DEFAULT NULL,
-  `project_end_date` varchar(40) DEFAULT NULL,
-  `user_id` int(5) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_projects_users_idx` (`user_id`),
-  CONSTRAINT `fk_projects_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `projects`
 --
 
 LOCK TABLES `projects` WRITE;
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
-INSERT INTO `projects` VALUES (10,'Asem\'s house','King Faisal Rd','2022-05-03','2022-06-01',9);
+INSERT INTO `projects` VALUES (10,'Asem\'s house','King Faisal Rd','2022-05-03','2022-06-01',9),(11,'Asem\'s house','King Faisal Rd','2022-05-09','2022-05-10',10);
 /*!40000 ALTER TABLE `projects` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `purchases`
---
-
-DROP TABLE IF EXISTS `purchases`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `purchases` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `purchase_name` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
-  `purchase_details` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
-  `purchase_type` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
-  `purchase_unit_price` decimal(20,2) DEFAULT NULL,
-  `purchase_unit_quantity` int(60) DEFAULT NULL,
-  `purchase_total` decimal(20,2) DEFAULT NULL,
-  `project_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_purchases_projects1_idx` (`project_id`),
-  CONSTRAINT `fk_purchases_projects1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `purchases`
@@ -226,30 +80,9 @@ CREATE TABLE `purchases` (
 
 LOCK TABLES `purchases` WRITE;
 /*!40000 ALTER TABLE `purchases` DISABLE KEYS */;
-INSERT INTO `purchases` VALUES (8,'Asem','nhfgjgdj','cash',31.00,231321,7170951.00,10);
+INSERT INTO `purchases` VALUES (8,'Asem','nhfgjgdj','cash',31.00,231321,7170951.00,10),(9,'gegdf','hnhsdfdfhsdhsdh','cash',21.00,32,672.00,10),(10,'hh','ngnsfgns','postponed',34.00,23,782.00,10);
 /*!40000 ALTER TABLE `purchases` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Table structure for table `statements`
---
-
-DROP TABLE IF EXISTS `statements`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `statements` (
-  `id` int(11) NOT NULL,
-  `statement_unit` varchar(200) DEFAULT NULL,
-  `statement_unit_price` decimal(20,2) DEFAULT NULL,
-  `statement_unit_quantity` varchar(200) DEFAULT NULL,
-  `contractor_id` int(60) DEFAULT NULL,
-  `employee_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `contractor_id_idx` (`contractor_id`),
-  KEY `fk_statements_employees1_idx` (`employee_id`),
-  CONSTRAINT `fk_statements_employees1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `statements`
@@ -261,30 +94,18 @@ LOCK TABLES `statements` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int(5) NOT NULL AUTO_INCREMENT,
-  `name` varchar(200) NOT NULL,
-  `username` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Dumping data for table `users`
 --
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (9,'Asem','asem@alhaidary','$2b$10$tbD1S.2PcxtPOblQ6wwRZ.KdMJtBIhvpEEam4M8tcmRgoyBsYONHu');
+INSERT INTO `users` VALUES (9,'Asem','asem@alhaidary','$2b$10$tbD1S.2PcxtPOblQ6wwRZ.KdMJtBIhvpEEam4M8tcmRgoyBsYONHu'),(10,'Ehab','eh@E','$2b$10$VlFLJNhu.yO9cOj.MzQmg.tBL2/GAtJMZSSF/8XmsnY06bP54xokO');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'building_management'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -295,4 +116,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-08 21:38:26
+-- Dump completed on 2022-05-09  0:44:23
