e29c3f2f43c8b54414f75f455795d3c41d4094ea
EhabTawfiq <104732987+EhabTawfiq@users.noreply.github.com>
2022-05-08 21:39:23 +0300

    تعديل على النوع البياني

--- db/buildingDB.sql
+++ db/buildingDB.sql
@@ -24,13 +24,13 @@ DROP TABLE IF EXISTS `contractors`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contractors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `contractor_name` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `contractor_work_details` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
-  `contractor_work_unit` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `contractor_work_quantity` int(11) DEFAULT NULL,
-  `contractor_unit_price` decimal(10,2) DEFAULT NULL,
-  `contractor_work_total` decimal(10,2) DEFAULT NULL,
-  `project_id` int(11) NOT NULL,
+  `contractor_name` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `contractor_work_details` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `contractor_work_unit` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `contractor_work_quantity` int(50) DEFAULT NULL,
+  `contractor_unit_price` decimal(40,2) DEFAULT NULL,
+  `contractor_work_total` decimal(60,2) DEFAULT NULL,
+  `project_id` int(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_contractors_projects1_idx` (`project_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
@@ -55,13 +55,13 @@ DROP TABLE IF EXISTS `employees`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `employees` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `employee_name` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `employee_job` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `employee_phone_num` varchar(13) CHARACTER SET utf8 DEFAULT NULL,
-  `employee_day_price` decimal(11,2) DEFAULT NULL,
+  `employee_name` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
+  `employee_job` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `employee_phone_num` varchar(300) CHARACTER SET utf8 DEFAULT NULL,
+  `employee_day_price` decimal(60,2) DEFAULT NULL,
   `employee_start_date` date DEFAULT NULL,
   `employee_end_date` date DEFAULT NULL,
-  `project_id` int(11) NOT NULL,
+  `project_id` int(100) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_employees_projects1_idx` (`project_id`),
   CONSTRAINT `fk_employees_projects1` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
@@ -86,15 +86,14 @@ DROP TABLE IF EXISTS `extras`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `extras` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `extra_time` decimal(10,2) DEFAULT NULL,
-  `extra_time_price` decimal(10,2) DEFAULT NULL,
-  `extra_total_price` decimal(10,2) DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  `extra_time` decimal(60,2) DEFAULT NULL,
+  `extra_time_price` decimal(60,2) DEFAULT NULL,
+  `extra_total_price` decimal(60,2) DEFAULT NULL,
   `employee_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_extras_employees1_idx` (`employee_id`),
-  CONSTRAINT `fk_extras_employees1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  KEY `fk_extras_employees1_idx` (`employee_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,9 +147,9 @@ DROP TABLE IF EXISTS `managers`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `managers` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `manager_name` varchar(45) DEFAULT NULL,
+  `manager_name` varchar(200) DEFAULT NULL,
   `manager_outlay_details` varchar(200) DEFAULT NULL,
-  `manager_outlay_amount` decimal(10,2) DEFAULT NULL,
+  `manager_outlay_amount` decimal(60,2) DEFAULT NULL,
   `manager_outlay_date` date DEFAULT NULL,
   `project_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
@@ -178,8 +177,8 @@ DROP TABLE IF EXISTS `projects`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `projects` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `project_name` varchar(40) DEFAULT NULL,
-  `project_address` varchar(40) DEFAULT NULL,
+  `project_name` varchar(100) DEFAULT NULL,
+  `project_address` varchar(2000) DEFAULT NULL,
   `project_start_date` varchar(40) DEFAULT NULL,
   `project_end_date` varchar(40) DEFAULT NULL,
   `user_id` int(5) NOT NULL,
@@ -208,11 +207,11 @@ DROP TABLE IF EXISTS `purchases`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `purchases` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `purchase_name` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `purchase_details` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
-  `purchase_type` varchar(45) CHARACTER SET utf8 DEFAULT NULL,
+  `purchase_name` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `purchase_details` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
+  `purchase_type` varchar(200) CHARACTER SET utf8 DEFAULT NULL,
   `purchase_unit_price` decimal(20,2) DEFAULT NULL,
-  `purchase_unit_quantity` int(11) DEFAULT NULL,
+  `purchase_unit_quantity` int(60) DEFAULT NULL,
   `purchase_total` decimal(20,2) DEFAULT NULL,
   `project_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
@@ -240,10 +239,10 @@ DROP TABLE IF EXISTS `statements`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `statements` (
   `id` int(11) NOT NULL,
-  `statement_unit` varchar(45) DEFAULT NULL,
+  `statement_unit` varchar(200) DEFAULT NULL,
   `statement_unit_price` decimal(20,2) DEFAULT NULL,
-  `statement_unit_quantity` varchar(45) DEFAULT NULL,
-  `contractor_id` int(11) DEFAULT NULL,
+  `statement_unit_quantity` varchar(200) DEFAULT NULL,
+  `contractor_id` int(60) DEFAULT NULL,
   `employee_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `contractor_id_idx` (`contractor_id`),
@@ -270,8 +269,8 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `username` varchar(20) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `username` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
@@ -286,10 +285,6 @@ LOCK TABLES `users` WRITE;
 INSERT INTO `users` VALUES (9,'Asem','asem@alhaidary','$2b$10$tbD1S.2PcxtPOblQ6wwRZ.KdMJtBIhvpEEam4M8tcmRgoyBsYONHu');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'building_management'
---
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -300,4 +295,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-07 23:50:26
+-- Dump completed on 2022-05-08 21:38:26
