4374afb450bc3ab5f4211eee0dc672b847225235
AsemAlhaidary <asemalhaidary.2001@gmail.com>
2022-06-06 00:47:26 +0300

    Complete design reports for employees & contractors

--- routes/reports.js
+++ routes/reports.js
@@ -5,28 +5,10 @@ const pdf = require('pdf-creator-node');
 const path = require('path');
 const database = require('../models/database');
 const usefulTools = require('../public/js/tools');
-const logo = require('../public/js/base64');
 const router = express.Router();
 
 const dbService = database.getDbServiceInstance();
 const tools = usefulTools.getToolsInstance();
-
-const html = fs.readFileSync(path.join(__dirname, '../views/reports/template.html'), 'utf-8');
-const filename = 'report' + Math.random() + '_doc' + '.pdf';
-
-const reportOptions = {
-  formate: 'A4',
-  orientation: 'portrait',
-  border: '8mm',
-  header: {
-    height: '15mm',
-    contents: '<h4 style="color: red; font-size: 20px; font-weight: 800; text-align: center;">بسم الله الرحمن الرحيم</h4>'
-  },
-  footer: {
-    height: '20mm',
-    contents: {}
-  }
-}
 
 router.post('/employeesreport/:projectId', security.checkAuthenticated, async (req, res) => {
   const { projectId } = req.params;
@@ -44,25 +26,79 @@ router.post('/employeesreport/:projectId', security.checkAuthenticated, async (r
   total = JSON.parse(JSON.stringify(total[0]));
   project = JSON.parse(JSON.stringify(project[0]));
 
-  employees.forEach(employee => {
+  employees.forEach((employee, i) => {
+    employee.id = i + 1;
     employee.employee_start_date = tools.getStandardDate(employee.employee_start_date);
     employee.employee_end_date = tools.getStandardDate(employee.employee_end_date);
   });
 
+  let data = {
+    project: project,
+    title: 'إجمالي الأيدي العاملة',
+    items: employees,
+    total: total
+  }
+
+  createPDF('employees.html', data, '/employees/' + projectId, res);
+});
+
+router.post('/contractorsreport/:projectId', security.checkAuthenticated, async (req, res) => {
+  const { projectId } = req.params;
+
+  const totalSql = 'SELECT SUM(contractor_work_total) sum FROM contractors WHERE project_id = ?';
+  const totalParams = [projectId];
+
+  const projectSql = 'SELECT * FROM projects WHERE id = ?';
+  const projectParams = [projectId];
+
+  let contractors = await dbService.getContractorsByProjectId(projectId);
+  let total = await dbService.runQuery(totalSql, totalParams);
+  let project = await dbService.runQuery(projectSql, projectParams);
+
+  total = JSON.parse(JSON.stringify(total[0]));
+  project = JSON.parse(JSON.stringify(project[0]));
+
+  contractors.forEach((contractor, i) => {
+    contractor.id = i + 1;
+  });
+
+  let data = {
+    project: project,
+    title: 'كشف حساب مقاولين',
+    items: contractors,
+    total: total
+  }
+
+  createPDF('contractors.html', data, '/contractors/' + projectId, res);
+});
+
+function createPDF(templateFile, data, srcPath, res) {
+  const html = fs.readFileSync(path.join(__dirname, '../views/reports/' + templateFile), 'utf-8');
+  const filename = 'report' + Math.random() + '_doc' + '.pdf';
+  
+  const reportOptions = {
+    formate: 'A4',
+    orientation: 'portrait',
+    border: '4mm',
+    header: {
+      height: '',
+      contents: ''
+    },
+    footer: {
+      height: '',
+      contents: {}
+    }
+  }
+
   const document = {
     html: html,
-    data: {
-      project: project,
-      items: employees,
-      total: total,
-      logoImg: logo
-    },
+    data: data,
     path: './docs/' + filename
   };
 
   pdf.create(document, reportOptions)
-    .then(res.redirect('/employees/' + projectId))
+    .then(res.redirect(srcPath))
     .catch(err => { console.log(err) });
-});
+}
 
 module.exports = router;
